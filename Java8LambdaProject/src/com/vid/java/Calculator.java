029edeabe6f894313fbbb0ae752a41dbe5c240c1
vidyantit <vidyantsingh.developer@gmail.com>
2023-03-05 00:34:34 +0530

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I5774d0889617a9799c4a25758416a58d88d8d8aa

--- Java8LambdaProject/src/com/vid/java/Calculator.java
+++ Java8LambdaProject/src/com/vid/java/Calculator.java
@@ -1,6 +1,7 @@
 package com.vid.java;
 
 public interface Calculator {
-void switchOn();
+//void switchOn();
+	void sum(int x,int y);
 }
 
--- Java8LambdaProject/src/com/vid/java/CalculatorImpl.java
+++ Java8LambdaProject/src/com/vid/java/CalculatorImpl.java
@@ -4,9 +4,11 @@ public class CalculatorImpl {
 	
 	public static void main(String args[]) {
 		
-		Calculator calculator=()-> {
-			System.out.println("This is SwithcOn");	
-		};
-		calculator.switchOn();
+		/*
+		 * Calculator calculator=()-> { System.out.println("This is SwithcOn"); };
+		 * calculator.switchOn();
+		 */
+		Calculator calculator=(x,y)->System.out.println("The sum:"+(x+y));	
+	calculator.sum(4,5);
 	}
-}
+} 	
